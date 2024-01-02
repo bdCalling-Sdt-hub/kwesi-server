933090fe3e20ed5f1ba1f1aca0657a2af3ab30c9
Shehabuddin-Tushar <freelancerrtushar@gmail.com>
2024-01-02 12:11:28 +0600

    privacy policy web view

---
+++ resources/views/privacy/privacy.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Privacy policy</title>
+</head>
+<body>
+    <div style="width:900px;">
+        <h1>{{$result['title']}}</h1>
+        <p style="font-size:20px;text-align:justify">{{$result['description']}}</p>
+    </div>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Models\Privacy;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +15,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/privacy-policy',function(){
+    $result=Privacy::first();
+   
+    return view('privacy.privacy',compact('result'));
 });
